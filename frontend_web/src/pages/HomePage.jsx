89e1e32c51b3169f8fb356418a07f7a201de5d88
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-09-09 23:38:16 +0530

    Create the HomePage.jsx

---
+++ frontend_web/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainBanner from '../components/MainBanner'
+import Categories from '../components/Categories'
+
+const HomePage = () => {
+  return (
+    <div className='mt-10'>
+        <MainBanner />
+        <Categories />
+    </div>
+  )
+}
+
+export default HomePage
