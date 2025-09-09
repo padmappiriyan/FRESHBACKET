befc2fb48d433444c98d00ff513d26e1dd4b60f9
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-09-09 23:35:34 +0530

    Updated the App.jsx

--- frontend_web/src/App.jsx
+++ frontend_web/src/App.jsx
@@ -1,9 +1,21 @@
+import { Routes,Route, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+
 
 const App = () => {
+  const isSellerPath=useLocation().pathname.includes("seller");
   return (
     <div>
-      <Navbar/>
+      {!isSellerPath && (
+             <Navbar/>
+      )}
+      
+      <div className={`${isSellerPath ? "" :"px-6 md:px-16 lx:px-24"}`} >
+        <Routes>
+          <Route path='/' element={<HomePage/>}></Route>
+        </Routes>
+      </div>
     </div>
   )
 }
