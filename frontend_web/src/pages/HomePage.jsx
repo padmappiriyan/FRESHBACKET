c4538c76b21e8c2f1cde59564e9437ae6d7d896b
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-09-12 21:43:36 +0530

    Update the Home Page

--- frontend_web/src/pages/HomePage.jsx
+++ frontend_web/src/pages/HomePage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import MainBanner from '../components/MainBanner'
 import Categories from '../components/Categories'
 import BestSeller from '../components/BestSeller'
+import BottomBanner from '../components/BottomBanner'
+import NewsLetter from '../components/NewsLetter'
 
 const HomePage = () => {
   return (
@@ -9,6 +11,8 @@ const HomePage = () => {
         <MainBanner />
         <Categories />
         <BestSeller />
+        <BottomBanner />
+        <NewsLetter />
     </div>
   )
 }
